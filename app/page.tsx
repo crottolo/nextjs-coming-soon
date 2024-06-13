871c88914e9cb9d36b4a49991d6465333dbe1722
Robby <crotti.roberto@gmail.com>
2024-06-13 23:51:16 +0200

    package.json for coolify

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { AuroraBackground } from '@/components/ui/aurora-background';
-// import React from 'react';
+import React from 'react';
 import { motion } from "framer-motion";
 import Link from "next/link";
 
-export function AuroraBackgroundDemo() {
+const AuroraBackgroundDemo = () => {
   return (
 
     <AuroraBackground>
@@ -32,35 +32,13 @@ export function AuroraBackgroundDemo() {
   );
 }
 
-const Aurora = () => {
+export default function Aurora() {
   return (
     <>
       <div className="w-full h-screen">
         <AuroraBackgroundDemo />
       </div>
-      {/* <AuroraBackground>
-        <motion.div
-          initial={{ opacity: 0.0, y: 40 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="relative flex flex-col gap-4 items-center justify-center px-4"
-        >
-          <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-            Miss Rent Sta arrivando
-        </div>
-          <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-            And this, is chemical burn.
-          </div>
-          <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-            Debug now
-          </button>
-        </motion.div>
-      </AuroraBackground> */}
+
     </>
   );
 }
-export default Aurora;
